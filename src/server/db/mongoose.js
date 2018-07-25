268215abbc50c007fd8b18cd86294535c459a121
Zeljko Olah <olah.zeljko@gmail.com>
2018-07-25 14:26:48 +0200

    Configure mongo lab with fallback

--- src/server/db/mongoose.js
+++ src/server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/MM', {
   useNewUrlParser: true
 })
   .then(() => {
