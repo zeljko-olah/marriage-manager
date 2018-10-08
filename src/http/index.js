3340592c47d931c1cc622628beb5fb6ccb14829d
Zeljko Olah <olah.zeljko@gmail.com>
2018-10-08 16:08:17 +0200

    change api to heroku url for production

--- src/http/index.js
+++ src/http/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 let url = process.env.NODE_ENV === 'development' ?
-  'http://localhost:3231/' : ''
+  'http://localhost:3231/' : 'https://vast-falls-59724.herokuapp.com/'
 
 const instance = axios.create({
   baseURL: url,
